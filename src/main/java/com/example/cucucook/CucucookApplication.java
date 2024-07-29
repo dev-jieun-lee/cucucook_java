5e694fac2c72f9160aa34fe246167922d763ec40
dev-jieun-lee <dev.jieun.lee@gmail.com>
2024-07-29 15:58:59 +0900

    first commit

---
+++ src/main/java/com/example/cucucook/CucucookApplication.java
@@ -0,0 +1,13 @@
+package com.example.cucucook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucucookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucucookApplication.class, args);
+	}
+
+}
