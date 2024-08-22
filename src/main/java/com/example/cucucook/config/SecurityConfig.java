786a6222c0d19f932821e5c9d79b8c66b7861710
dev-jieun-lee <dev.jieun.lee@gmail.com>
2024-08-22 16:47:06 +0900

    JWT인증 로그인 커밋

--- src/main/java/com/example/cucucook/config/SecurityConfig.java
+++ src/main/java/com/example/cucucook/config/SecurityConfig.java
@@ -9,9 +9,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 @Configuration
 public class SecurityConfig {
