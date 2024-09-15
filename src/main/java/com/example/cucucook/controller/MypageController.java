1ea9485f0e4b551cc18276f1b766dadcc10d0c84
dev-jieun-lee <dev.jieun.lee@gmail.com>
2024-09-15 15:34:05 +0900

    로그인 성공 실패 로그확인

--- src/main/java/com/example/cucucook/controller/MypageController.java
+++ src/main/java/com/example/cucucook/controller/MypageController.java
@@ -5,18 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cucucook.domain.Member;
 import com.example.cucucook.service.MypageService;
 
 @RestController
+@RequestMapping("/api/mypage")
 public class MypageController {
 
     @Autowired
     private MypageService mypageService;
 
-    @PostMapping("/api/verify-password")
+    @PostMapping("/verify-password")
     public ResponseEntity<String> verifyPassword(@RequestBody Member member) {
         boolean isVerified = mypageService.verifyPassword(member.getUserId(), member.getPassword());
 
