71c86763aaf4f177d1c756bbcccc994c9ac6653d
haerim <gofla333@naver.com>
2024-09-23 12:44:34 +0900

    1.  log4j 재설정 2. env 분리 3. 레시피 등록 / 수정 / 삭제 구현 4. 레시피 댓글 등록 / 수정 / 삭제 구현 5. api 응답 자바 변경 6. 파일 핸들러 추가

--- src/main/java/com/example/cucucook/domain/MemberRecipe.java
+++ src/main/java/com/example/cucucook/domain/MemberRecipe.java
@@ -2,6 +2,8 @@ package com.example.cucucook.domain;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,55 +13,83 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MemberRecipe implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    //레시피아이디
+    // 레시피아이디
     private String recipeId;
 
-    //회원고유번호
+    // 회원고유번호
     private int memberId;
 
-    //레시피 카테고리 아이디
+    // 레시피 카테고리 아이디
     private String recipeCategoryId;
 
-    //제목
+    // 제목
     private String title;
 
-    //조리방법
+    // 조리방법 카테고리 아이디
+    private String recipeMethodId;
+
+    // 조리방법
     private String method;
 
-    //이미지 아이디
+    // 조리방법(영문)
+    private String methodEn;
+
+    // 이미지 아이디
     private String imgId;
 
-    //분량(몇인분)
+    // 분량(몇인분)
     private int serving;
 
-    //난이도
+    // 레벨 카테고리 아이디
+    private String recipeLevelId;
+
+    // 난이도
     private String level;
 
-    //조리시간
+    // 난이도(영문)
+    private String levelEn;
+
+    // 조리시간
     private String time;
 
-    //열량
+    // 열량
     private String calory;
 
-    //팁
+    // 팁
     private String tip;
 
-    //등록일
+    // 등록일
     private String regDt;
 
-    //수정일
+    // 수정일
     private String uptDt;
 
-    //조회수
+    // 조회수
     private int viewCount;
 
-    //게시판 카테고리에서 조인해서 가져온 카테고리명
+    // 댓글수
+    private String commentCount;
+
+    // 댓글평점
+    private String commentRate;
+
+    // 좋아요 수
+    private int likeCount;
+
+    // 게시판 카테고리에서 조인해서 가져온 카테고리명
     private String recipeCategoryName;
 
-    //조인해서 가져오는 회원 정보
+    // 게시판 카테고리에서 조인해서 가져온 카테고리명(영문)
+    private String recipeCategoryNameEn;
+
+    // 조인해서 가져오는 회원 정보
     private Member member;
+
+    // 조인해서 가져오는 이미지 정보
+    private MemberRecipeImages memberRecipeImages;
 }
--- src/main/java/com/example/cucucook/domain/RecipeCategory.java
+++ src/main/java/com/example/cucucook/domain/RecipeCategory.java
@@ -11,9 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RecipeCategory {
 
-    //레시피 카테고리 아이디
+    // 레시피 카테고리 아이디
     private String recipeCategoryId;
-    //레시피 카테고리명
+    // 레시피 카테고리명
     private String name;
-
+    // 레시피 카테고리영문명
+    private String nameEn;
+    // 구분(카테고리:C/조리방법:M/난이도:L)
+    private String division;
+    // 카테고리 별 레시피 수
+    private String count;
 }
