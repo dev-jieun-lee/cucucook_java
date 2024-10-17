e5da798e40f346c004648bd9dacf3447545373ac
haerim <gofla333@naver.com>
2024-10-17 23:05:45 +0900

    CrossOrigin 제거

--- src/main/java/com/example/cucucook/controller/SocialLoginController.java
+++ src/main/java/com/example/cucucook/controller/SocialLoginController.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,7 +31,6 @@ import com.example.cucucook.service.SocialLoginService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/auth")
 public class SocialLoginController {
